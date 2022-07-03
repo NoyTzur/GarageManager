cbe06ae5cb2b3497bf84da384aa1e779fb7e9022
Noy <noycohen133@gmail.com>
2022-07-03 19:22:37 +0300

    upload project

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleUI
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            GarageManager garage = new GarageManager();
+            garage.RunGarage();
+        }
+    }
+}
